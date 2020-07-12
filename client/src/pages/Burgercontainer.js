94b02889158f2a6f4f4242a17a8dafaa467456c8
mattkrebs1974 <mattkrebsemail@gmail.com>
2020-07-11 22:05:23 -0400

    simple edit

--- client/src/pages/Burgercontainer.js
+++ client/src/pages/Burgercontainer.js
@@ -5,17 +5,20 @@ class Burgercontainer extends Component {
      
         
         return (
-            
-
-            <ul>
-             {this.props.burgersmade.map((info)=>
-                
-                
-                <li>{info.entry} Burger <button id={info._id} onClick={( )=>this.props.changetoeaten(info._id)}>Eat it</button></li>)}
-           
-            </ul>
-            
-        )
+          <ul>
+            {this.props.burgersmade && this.props.burgersmade.map((info) => (
+              <li>
+                {info.entry} Burger{" "}
+                <button
+                  id={info._id}
+                  onClick={() => this.props.changetoeaten(info._id)}
+                >
+                  Eat it
+                </button>
+              </li>
+            ))}
+          </ul>
+        );
           
     }
   
