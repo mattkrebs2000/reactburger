306a3c916d2c19137d39586c459bb1ba59a56cdc
mattkrebs1974 <mattkrebsemail@gmail.com>
2021-02-17 11:40:22 -0500

    Updates

---
+++ precache-manifest.055fd37b5e9d28bdcb2bc28899cc41ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dafc141655229b05b3a",
+    "url": "/reactburger/static/css/main.49bd5e4d.chunk.css"
+  },
+  {
+    "revision": "8dafc141655229b05b3a",
+    "url": "/reactburger/static/js/main.02d9ed8d.chunk.js"
+  },
+  {
+    "revision": "e084d7f1cd618e90f2fd",
+    "url": "/reactburger/static/js/runtime~main.2add4996.js"
+  },
+  {
+    "revision": "03a040ba2d399c9144b1",
+    "url": "/reactburger/static/js/2.106ba644.chunk.js"
+  },
+  {
+    "revision": "775ad4f9ec7c8c8845d6d02c72a720aa",
+    "url": "/reactburger/index.html"
+  }
+];
